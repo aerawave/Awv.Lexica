5f5b290cdd5f9ae77334aabed93ac0b2c1380279
Joshua <jaxkinzer@aerawave.com>
2020-02-20 16:19:41 -0500

    Made it so that Compositions implemented IList<ILexigram> instead of inheriting List<ILexigram>.

--- Awv.Lexica/Compositional/Composition.cs
+++ Awv.Lexica/Compositional/Composition.cs
@@ -1,13 +1,18 @@
 ﻿using Awv.Lexica.Compositional.Interface;
 using Awv.Lexica.Compositional.Lexigrams.Interface;
+using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Awv.Lexica.Compositional
 {
-    public class Composition : List<ILexigram>, ILexigram
+    public class Composition : IList<ILexigram>, ILexigram
     {
+        private List<ILexigram> InternalList { get; set; } = new List<ILexigram>();
+
         public string Joiner { get; set; } = "";
+
+
         /// <summary>
         /// Builds a string, joined with the <see cref="Joiner"/> from each of the child <see cref="ILexigram"/> values with the the <paramref name="engine"/>.
         /// </summary>
@@ -31,10 +36,26 @@ namespace Awv.Lexica.Compositional
             }
             return string.Join(Joiner, built);
         }
-
         public virtual object GetValue(ICompositionEngine engine)
             => Build(engine);
 
         public override string ToString() => string.Join(Joiner, this);
+
+        #region IList<ILexigram> Forwarding Implementation
+        public ILexigram this[int index] { get => InternalList[index]; set => InternalList[index] = value; }
+        public int Count => InternalList.Count;
+        public bool IsReadOnly => false;
+        public void Add(ILexigram item) => InternalList.Add(item);
+        public void Clear() => InternalList.Clear();
+        public bool Contains(ILexigram item) => InternalList.Contains(item);
+        public void CopyTo(ILexigram[] array, int arrayIndex) => InternalList.CopyTo(array, arrayIndex);
+        public IEnumerator<ILexigram> GetEnumerator() => InternalList.GetEnumerator();
+        public int IndexOf(ILexigram item) => InternalList.IndexOf(item);
+        public void Insert(int index, ILexigram item) => InternalList.Insert(index, item);
+        public bool Remove(ILexigram item) => InternalList.Remove(item);
+        public void RemoveAt(int index) => InternalList.RemoveAt(index);
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+        #endregion
+
     }
 }
